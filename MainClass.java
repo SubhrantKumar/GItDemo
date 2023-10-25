0c8f0fc3e7d75524c983c924efec1969128fe62a
Subhrant <sahusubhranta@gmail.com>
2023-10-25 12:31:16 +0530

    pushed main class

---
+++ MainClass.java
@@ -0,0 +1,12 @@
+package Basics;
+
+public class MainClass {
+static int x = 232;
+
+public static void main(String[] args) {
+
+//	MainClass m = new MainClass();
+	
+	System.out.println(MainClass.x);
+}
+}
