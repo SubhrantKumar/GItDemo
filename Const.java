b7f5561b2ae870207ca0977b1489a0faf3c26bd8
Subhrant <sahusubhranta@gmail.com>
2023-10-25 11:44:42 +0530

    Initial commit

---
+++ Const.java
@@ -0,0 +1,19 @@
+package Basics;
+
+public class Const {
+	
+	public Const() {
+		System.out.println("line 1");
+		
+	}
+
+	void man(int m)
+	{
+		System.out.println("Age is "+m);
+	}
+	public static void main(String[] args) {
+	Const c = new Const();
+	
+	
+}
+}
